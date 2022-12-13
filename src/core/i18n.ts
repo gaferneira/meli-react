5d451c0b637a47fc0d50314bd5ce6c108889047a
Gabriel Neira <gabriel.neira@sigmaconnectivity.se>
2022-12-13 15:37:59 +0100

    Change library

--- src/core/i18n.ts
+++ src/core/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import XHR from "i18next-xhr-backend";
+import XHR from "i18next-http-backend";
 
 const fallbackLng = ["en"];
 const supportedLngs = ["en", "es"];
